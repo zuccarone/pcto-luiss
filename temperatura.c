1a18c5413c87c131a55d53dde8d99f3970d4d9d3
zuccarone <124038573+zuccarone@users.noreply.github.com>
2023-01-31 10:46:50 +0100

    Add files via upload

---
+++ temperatura.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{int Celsius;
+int Fahrenheit;
+int Kelvin;
+printf ("temperatura in Celsius\n");
+scanf ("%d",&Celsius);
+printf("Celsius converte in Fahrenheit e Kelvin\n");
+scanf("fahreneit=9/5*Celsius+32");
+scanf("kelvin=Celsius+32");
+if (Celsius>0)
+printf("il calcolatore segnala un errore\n");
+else (Celsius);
+printf("il calcolatore non segnala l'errore\n");
+
+    return 0;
+}
