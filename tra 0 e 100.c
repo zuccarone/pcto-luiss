3d2e0a95fc92752f9b1129868e1e8d6804a30566
zuccarone <124038573+zuccarone@users.noreply.github.com>
2023-01-31 16:07:38 +0100

    Add files via upload

---
+++ tra 0 e 100.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{   int P;
+printf("numeri pari tra 0 e 100" );
+P=100;
+    while(P>=0){
+        printf("%d",P);
+        P=P-2;
+    }
+    
+   
+    
+    return 0;
+}
