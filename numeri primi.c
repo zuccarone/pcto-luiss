c74cb71750c6c41b52fa61387a275ede8ea8c810
zuccarone <124038573+zuccarone@users.noreply.github.com>
2023-01-31 14:45:04 +0100

    Add files via upload

---
+++ numeri primi.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{   int N1;
+     printf ("inserisci N1\n");
+    scanf("%d",&N1);
+    if (N1%N1==1 || N1%1==N1)
+    printf ("N1 è un numero primo\n");
+    else 
+    printf("N1 non è un numero primo\n");
+    return 0;
+}
