954d92b7a4818261ff1b5c2769cb97c38022f56b
zuccarone <124038573+zuccarone@users.noreply.github.com>
2023-02-02 15:16:22 +0100

    Add files via upload

---
+++ str char.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{char str[] = "lazio cacca";
+int i=0;
+    printf("%s",str);
+while(str[i]!=0)
+{
+    i=i+1;
+    
+}
+printf ("stampare la i %d",i);
+
+
+
+
+    return 0;
+}
